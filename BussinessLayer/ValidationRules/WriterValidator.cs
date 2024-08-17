70af96261fedd917bd82fb3ca83103c880f71544
Eren İlgün <eren.ilgn.10@gmail.com>
2024-08-17 12:33:35 +0300

    12:33-17.08.2024

---
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrate;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessLayer.ValidationRules
+{
+    public class WriterValidator:AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar İsmi boş bırakılamaz.");
+            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar İsmi en az 2 karakter olmalıdır.");
+            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar İsmi en fazla 50 karakter olmalıdır.");
+            RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş bırakılamaz.");
+            RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail kısmı boş bırakılamaz.");
+        }
+    }
+}
