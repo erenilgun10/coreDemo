12ddf7e1e49fc30b315f41b4d9f3156f4512dc10
Eren İlgün <eren.ilgn.10@hotmail.com>
2024-08-18 19:01:14 +0300

    18.08.2024

--- BussinessLayer/ValidationRules/WriterValidator.cs
+++ BussinessLayer/ValidationRules/WriterValidator.cs
@@ -13,10 +13,12 @@ namespace BussinessLayer.ValidationRules
         public WriterValidator()
         {
             RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar İsmi boş bırakılamaz.");
-            RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar İsmi en az 2 karakter olmalıdır.");
-            RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar İsmi en fazla 50 karakter olmalıdır.");
+            RuleFor(x => x.WriterName).NotEmpty().Length(5, 50).WithMessage("Yazar ismi en az 5 en fazla 50 karakter olmalıdır.");
+            //RuleFor(x => x.WriterName).MinimumLength(2).WithMessage("Yazar İsmi en az 2 karakter olmalıdır.");
+            //RuleFor(x => x.WriterName).MaximumLength(50).WithMessage("Yazar İsmi en fazla 50 karakter olmalıdır.");
             RuleFor(x => x.WriterPassword).NotEmpty().WithMessage("Şifre boş bırakılamaz.");
             RuleFor(x => x.WriterMail).NotEmpty().WithMessage("Mail kısmı boş bırakılamaz.");
+
         }
     }
 }
---
+++ coreDemo/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,20 @@
+﻿using BussinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace coreDemo.ViewComponents.Blog
+{
+    public class WriterLastBlog :ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
+
+
+        
+    }
+}
